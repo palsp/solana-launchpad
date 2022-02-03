84a96b5330ac692aa678508a6e9dd7ff06c016d5
Supasin Liulak <palsp2406@gmail.com>
2022-02-03 14:06:40 +0700

    feat: withdraw from pool

--- programs/solana-launchpad/src/lib.rs
+++ programs/solana-launchpad/src/lib.rs
@@ -8,14 +8,12 @@ use access::*;
 use account::{IdoTimes, PoolBumps};
 use context::*;
 use error::*;
-use processor::Processor;
 
 mod access;
 mod account;
 mod context;
 mod error;
 mod merkle_proof;
-mod processor;
 
 #[program]
 pub mod solana_launchpad {
@@ -33,38 +31,40 @@ pub mod solana_launchpad {
         merkle_root: Option<[u8; 32]>,
     ) -> ProgramResult {
         msg!("INITIALIZE POOL");
-        Processor::initialize_pool_token(
-            ctx,
-            ido_name,
-            bumps,
-            private_target_investment,
-            num_ido_tokens_public,
-            num_ido_tokens_private,
-            ido_times,
-            merkle_root,
-        )
-    }
+        let ido_account = &mut ctx.accounts.ido_account;
 
-    #[access_control(validate_ido_times(ido_times))]
-    pub fn initialize_pool_native(
-        ctx: Context<InitializePoolNative>,
-        ido_name: String,
-        bumps: PoolBumps,
-        private_target_investment: u64,
-        num_ido_tokens_public: u64,
-        num_ido_tokens_private: u64,
-        ido_times: IdoTimes,
-    ) -> ProgramResult {
-        msg!("INITIALIZE POOL NATIVE");
-        Processor::initialize_pool_native(
-            ctx,
-            ido_name,
-            bumps,
-            private_target_investment,
-            num_ido_tokens_public,
-            num_ido_tokens_private,
-            ido_times,
-        )
+        let name_bytes = ido_name.as_bytes();
+        let mut name_data = [b' '; 10];
+        name_data[..name_bytes.len()].copy_from_slice(name_bytes);
+        ido_account.ido_name = name_data;
+        ido_account.bumps = bumps;
+        ido_account.ido_authority = ctx.accounts.ido_authority.key();
+        ido_account.redeemable_mint = ctx.accounts.redeemable_mint.key();
+        ido_account.watermelon_mint = ctx.accounts.watermelon_mint.key();
+        ido_account.pool_watermelon = ctx.accounts.pool_watermelon.key();
+        ido_account.num_ido_tokens_public = num_ido_tokens_public;
+        ido_account.num_ido_tokens_private = num_ido_tokens_private;
+        ido_account.private_target_investment = private_target_investment;
+        // Only for pool token
+        ido_account.pool_usdc = ctx.accounts.pool_usdc.key();
+        ido_account.usdc_mint = ctx.accounts.usdc_mint.key();
+        ido_account.ido_times = ido_times;
+        if let Some(root) = merkle_root {
+            ido_account.merkle_root = root;
+        }
+        // Transfer Watermelon from ido_authority_watermelon to pool account.
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.ido_authority_watermelon.to_account_info(),
+            to: ctx.accounts.pool_watermelon.to_account_info(),
+            authority: ctx.accounts.ido_authority.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        let total_ido_tokens = (num_ido_tokens_public as u128)
+            .checked_add(num_ido_tokens_private as u128)
+            .unwrap();
+        token::transfer(cpi_ctx, total_ido_tokens as u64)?;
+        Ok(())
     }
 
     #[access_control(unrestricted_phase(&ctx.accounts.ido_account))]
@@ -173,62 +173,6 @@ pub mod solana_launchpad {
         Ok(())
     }
 
-    // #[access_control(deposit_phase(&ctx.accounts.ido_account))]
-    pub fn exchange_native_for_redeemable(
-        ctx: Context<ExchangeNativeForRedeemable>,
-        amount_in: u64,
-    ) -> ProgramResult {
-        msg!("EXCHANGE NATIVE FOR REDEEMABLE");
-        require!(
-            ctx.accounts.user_authority.lamports() >= amount_in,
-            ErrorCode::LowUsdc
-        );
-
-        msg!(
-            "TRANSFER {} FROM user_authority account to {} account",
-            amount_in,
-            ctx.accounts.pool_native.key()
-        );
-        let ix = anchor_lang::solana_program::system_instruction::transfer(
-            &ctx.accounts.user_authority.key(),
-            &ctx.accounts.pool_native.key(),
-            amount_in,
-        );
-
-        anchor_lang::solana_program::program::invoke(
-            &ix,
-            &[
-                ctx.accounts.user_authority.to_account_info(),
-                ctx.accounts.pool_native.clone(),
-                ctx.accounts.system_program.to_account_info(),
-            ],
-        )?;
-
-        let ido_account = &ctx.accounts.ido_account;
-
-        msg!("MINT REDEEMABLE TO USER");
-        // Mint redeemable to user
-        let ido_name = ido_account.ido_name.as_ref();
-        let seeds = &[
-            ido_name.trim_ascii_whitespace(),
-            &[ido_account.bumps.ido_account],
-        ];
-        let signer = &[&seeds[..]];
-
-        let cpi_accounts = MintTo {
-            mint: ctx.accounts.redeemable_mint.to_account_info(),
-            to: ctx.accounts.user_redeemable.to_account_info(),
-            authority: ido_account.to_account_info(),
-        };
-
-        let cpi_program = ctx.accounts.token_program.to_account_info();
-
-        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
-        token::mint_to(cpi_ctx, amount_in)?;
-
-        Ok(())
-    }
-
     #[access_control(withdraw_phase(&ctx.accounts.ido_account))]
     pub fn exchange_redeemable_for_watermelon(
         ctx: Context<ExchangeRedeemableForWatermelon>,
@@ -294,42 +238,33 @@ pub mod solana_launchpad {
             token::close_account(cpi_ctx)?;
         }
 
-        // Transfer Watermelon To User
         Ok(())
     }
 
-    pub fn withdraw_native(ctx: Context<WithdrawNative>, amount: u64) -> ProgramResult {
-        msg!("WITHDRAW NATIVE");
+    #[access_control(ido_is_over(&ctx.accounts.ido_account))]
+    pub fn withdraw_pool_usdc(ctx: Context<WithdrawPoolUsdc>) -> ProgramResult {
+        msg!("WITHDRAW POOL USDC");
 
-        require!(ctx.accounts.pool_native.lamports() >= amount, ErrorCode::A);
         let ido_name = ctx.accounts.ido_account.ido_name.as_ref();
+
         let seeds = &[
             ido_name.trim_ascii_whitespace(),
-            b"pool_native",
-            &[ctx.accounts.ido_account.bumps.pool_native],
+            &[ctx.accounts.ido_account.bumps.ido_account],
         ];
+
         let signer = &[&seeds[..]];
 
-        let ix = anchor_lang::solana_program::system_instruction::transfer(
-            &ctx.accounts.pool_native.key(),
-            &ctx.accounts.user_authority.key(),
-            amount,
-        );
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.pool_usdc.to_account_info(),
+            to: ctx.accounts.user_usdc.to_account_info(),
+            authority: ctx.accounts.ido_account.to_account_info(),
+        };
 
-        anchor_lang::solana_program::program::invoke_signed(
-            &ix,
-            &[
-                ctx.accounts.pool_native.clone(),
-                ctx.accounts.user_authority.to_account_info(),
-                ctx.accounts.system_program.to_account_info(),
-            ],
-            signer,
-        )?;
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
+        token::transfer(cpi_ctx, ctx.accounts.pool_usdc.amount)?;
 
         Ok(())
     }
-
-    // pub fn withdraw_usdc(ctx: Context<InitializePool>) -> ProgramResult {
-    //     Ok(())
-    // }
 }
